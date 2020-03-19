ee66ce7c6d6ed5fb94723b9b9340ebb88d341b9c
Srivatsan Murali <50999780+srivatsan18@users.noreply.github.com>
2020-03-19 10:19:41 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/scrosri.jpg`,
+        icon: `content/assets/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
